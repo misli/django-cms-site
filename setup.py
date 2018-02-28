1b52474069d6debf512cf35d4179c8c648f44f05
Jakub Dorňák <jakub.dornak@misli.cz>
2018-02-28 10:16:04 +0100

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as requir
 
 setup(
     name='django-cms-site',
-    version='1.1.0',
+    version='1.1.1',
     description='Default Django CMS site suitable for Docker',
     long_description=long_description,
     author='Jakub Dorňák',
